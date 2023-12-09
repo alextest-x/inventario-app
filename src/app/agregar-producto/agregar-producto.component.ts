7e133c363f45a94743427c94fa62079e0b0068fb
alextest-x <alexsnet01@gmail.com>
2023-12-08 23:51:22 -0600

    inventarios en angular & backend en spring

---
+++ src/app/agregar-producto/agregar-producto.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-agregar-producto',
+  templateUrl: './agregar-producto.component.html',
+  styleUrls: ['./agregar-producto.component.css']
+})
+export class AgregarProductoComponent {
+
+   producto: Producto = new Producto();
+
+
+   //enrutador dirige la respuesta al listado de producto ya que haya obtenido la respuesta del backend
+   constructor( private productoServicio: ProductoService,
+                private enrutador: Router){}
+
+    onSubmit(){
+     this.guardarProducto();
+    }
+
+
+
+    guardarProducto(){
+      this.productoServicio.agregarProducto(this.producto).subscribe(
+        {   //datos es el objeto que se mando a imprimir a la base de datos se cargan los datos
+            //y despues que recibimos la respuesta indica que ya se proceso la repuesta
+            //entonces redirigir l respuesta a a irLista
+
+          next: (datos) => {
+            this.irListarProducto();
+
+         },
+
+          error: (error: any) => {
+            console.log(error);
+
+          }
+        }
+      )
+    }
+
+
+    //usamos el enrutador para redirigir la respuesta y se cargen los productos de nuevo
+
+    irListarProducto(){
+      this.enrutador.navigate(['/productos'])
+
+    }
+
+
+
+}
---
+++ src/app/producto.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import{ HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Producto } from './producto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductoService {
+
+  private urlBase = "http://localhost:8080/inventario-app/productos"
+
+  constructor( private clienteHttp: HttpClient ) { }
+
+
+  obtenerProductosLista(): Observable<Producto[]>{
+  return this.clienteHttp.get<Producto[]>(this.urlBase);
+  }
+
+  //se recibe un objeto de tipo producto que el que se envia un la peticion de tipo post
+  //regresa un objeto de tipo observable
+  agregarProducto(producto: Producto): Observable<Object>{
+    return this.clienteHttp.post(this.urlBase, producto); //se envia producto
+  }
+
+  obtenerProductoPorId(id: number){
+    return this.clienteHttp.get<Producto>(`${this.urlBase}/${id}`);
+  }
+
+ //se recibe el id de tipo producto
+ //se recibe el objeto de tipo Producto donde trae lo valores del formulario
+ //los cambios ya actualizados
+
+ //el metodo regresa un tipo observable y regresa un objeto de tipo object
+ //con put enviamos el id y la informacion que viene del formulario con nueva actualizacion
+  editarProducto(id: number, producto: Producto): Observable<Object>{
+    return this.clienteHttp.put(`${this.urlBase}/${id}`, producto);
+  }
+
+  eliminarProducto(id: number):Observable<Object>{
+    return this.clienteHttp.delete(`${this.urlBase}/${id}`);
+  }
+
+}
